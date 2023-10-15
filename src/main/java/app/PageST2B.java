cfdc887fca656fcfbc7d7586e77669a6332a51a7
Nathan <s3993380@student.rmit.edu.au>
2023-10-15 12:11:23 +1100

    changed homepage to home

--- src/main/java/app/PageST2B.java
+++ src/main/java/app/PageST2B.java
@@ -49,12 +49,12 @@ public class PageST2B implements Handler {
                     <a href = '/'><img src='logo.png' class='top-image' alt='RMIT logo' height='70'></a>
                 </div>
                 <div class ='buttons'>
-                <a href='/'>Homepage</a>
-                <a href='mission.html'>About us</a>
-                <a href='page2A.html'>Sub Task 2.A</a>
-                <a href='page2B.html'>Sub Task 2.B</a>
-                <a href='page3A.html'>Sub Task 3.A</a>
-                <a href='page3B.html'>Sub Task 3.B</a>
+                    <a href='/'>Home</a>
+                    <a href='page2A.html'>Sub Task 2.A</a>
+                    <a href='page2B.html'>Sub Task 2.B</a>
+                    <a href='page3A.html'>Sub Task 3.A</a>
+                    <a href='page3B.html'>Sub Task 3.B</a>
+                    <a href='mission.html'>About us</a>
                 </div>
             </div>
         """;
