d7e0ef7282ac0f88551aa33083bfab4f7eb0f96c
Ariani Plukaard <s4055430@student.rmit.edu.au>
2023-10-15 15:36:02 +1100

    added bootstrap into index page

--- src/main/java/app/PageIndex.java
+++ src/main/java/app/PageIndex.java
@@ -31,10 +31,14 @@ public class PageIndex implements Handler {
         String html = "<html>";
 
         // Add some Header information
-        html = html + "<head>" + 
-               "<title>Homepage</title>";
+        html = html + "<head>" + """
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <title>Homepage</title>
+        """;
 
-        // Add some CSS (external file)
+        // Add some CSS (external file) and the Bootstrap CSS
+        html = html + "<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN' crossorigin='anonymous'>";
         html = html + "<link rel='stylesheet' type='text/css' href='common.css' />";
         html = html + "</head>";
 
@@ -147,7 +151,8 @@ public class PageIndex implements Handler {
         """;
         //TO DO: create FAQ
 
-        // Finish the HTML webpage
+        // Add Bootstrap JavaScript bundle and finish the HTML webpage
+        html = html + "<script src='https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js' integrity='sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL' crossorigin='anonymous'></script>";
         html = html + "</body>" + "</html>";
 
 
