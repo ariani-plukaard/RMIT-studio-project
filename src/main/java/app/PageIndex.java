f856a865a2c28432705e3aee0cf1fa5188b0ac72
Ariani Plukaard <s4055430@student.rmit.edu.au>
2023-10-15 15:48:32 +1100

    added a bootstrap carousel template to the homepage

--- src/main/java/app/PageIndex.java
+++ src/main/java/app/PageIndex.java
@@ -123,7 +123,38 @@ public class PageIndex implements Handler {
 
                 </div>
             </div>
-            """;
+        """;
+        
+        // Add HTML for the graphs carousel
+        //TO DO: add graphs instead of images on each slide
+        html = html + """
+            <div id="carouselGraphs" class="carousel slide" data-bs-theme="dark">
+                <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselGraphs" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselGraphs" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselGraphs" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="..." class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="..." class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="..." class="d-block w-100" alt="...">
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselGraphs" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselGraphs" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        """;
 
         // Footer
         html = html + """
