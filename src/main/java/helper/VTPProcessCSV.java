f12cac219d233e050e6ca36d78d903fdb9ce118b
Ariani Plukaard <s4055430@student.rmit.edu.au>
2023-10-16 14:36:20 +1100

    updating the helper file

--- src/main/java/helper/VTPProcessCSV.java
+++ src/main/java/helper/VTPProcessCSV.java
@@ -34,9 +34,105 @@ public class VTPProcessCSV {
    // MODIFY these to load/store to/from the correct locations
    private static final String DATABASE = "jdbc:sqlite:database/vtp.db";
    private static final String CSV_FILE = "database/lga_indigenous_status_by_age_by_sex_census_2016.csv";
-
+   private static final String LGAS_2016_CSV_FILE = "lgas_2016.csv";
 
    public static void main (String[] args) {
+      populatePopulationTable();
+      //populateLGATable(LGAS_2016_CSV_FILE, 2016);
+      
+   }
+
+   private static void populateLGATable(String filename, int year) {
+      // The following arrays define the order of columns in the INPUT CSV.
+      // The order of each array MUST match the order of the CSV.
+      // These are specific to the given file and should be changed for each file.
+      // This is a *simple* way to help you get up and running quickly wihout being confusing
+
+      // JDBC Database Object
+      Connection connection = null;
+
+      // We need some error handling.
+      try {
+         // Open A CSV File to process, one line at a time
+         // CHANGE THIS to process a different file
+         Scanner lineScanner = new Scanner(new File(filename));
+
+         // Read the first line of "headings"
+         String header = lineScanner.nextLine();
+         System.out.println("Heading row" + header + "\n");
+
+         // Connect to JDBC database
+         connection = DriverManager.getConnection(DATABASE);
+
+         // Read each line of the CSV
+         int row = 1;
+         while (lineScanner.hasNext()) {
+            // Always get scan by line
+            String line = lineScanner.nextLine();
+            
+            // Create a new scanner for this line to delimit by commas (,)
+            Scanner rowScanner = new Scanner(line);
+            rowScanner.useDelimiter(",");
+
+            // These indicies track which column we are up to
+            int indexStatus = 0;
+            int indexSex = 0;
+            int indexCategory = 0;
+
+            // Save the lga_code as we need it for the foreign key
+            String lgaCode = rowScanner.next();
+
+            // Skip lga_name
+            String lgaName = rowScanner.next();
+
+            // Go through the data for the row
+            // If we run out of categories, stop for safety (so the code doesn't crash)
+            while (rowScanner.hasNext() && indexCategory < category.length) {
+               String count = rowScanner.next();
+
+               // Prepare a new SQL Query & Set a timeout
+               Statement statement = connection.createStatement();
+
+               // Create Insert Statement
+               String query = "INSERT into Population VALUES ("
+                              + lgaCode + ","
+                              + year + ","
+                              + "'" + status[indexStatus] + "',"
+                              + "'" + sex[indexSex] + "',"
+                              + "'" + category[indexCategory] + "',"
+                              + count + ","
+                              + age_min[indexCategory] + ","
+                              + age_max[indexCategory]
+                              + ")";
+
+               // Execute the INSERT
+               System.out.println("Executing: " + query);
+               statement.execute(query);
+
+               // Update indices - go to next sex
+               indexSex++;
+               if (indexSex >= sex.length) {
+                  // Go to next status
+                  indexSex = 0;
+                  indexStatus++;
+
+                  if (indexStatus >= status.length) {
+                     // Go to next Category
+                     indexStatus = 0;
+                     indexCategory++;
+                  }
+               }
+               row++;
+            }
+         }
+
+      } catch (Exception e) {
+         e.printStackTrace();
+      }      
+
+   }
+
+   private static void populatePopulationTable() {
       // The following arrays define the order of columns in the INPUT CSV.
       // The order of each array MUST match the order of the CSV.
       // These are specific to the given file and should be changed for each file.
@@ -183,5 +279,7 @@ public class VTPProcessCSV {
       } catch (Exception e) {
          e.printStackTrace();
       }      
+
    }
+   
 }
