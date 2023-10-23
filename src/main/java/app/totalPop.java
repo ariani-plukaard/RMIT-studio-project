69bfbf7825b92f6c4cee176edbfed2d71b357118
Nathan <s3993380@student.rmit.edu.au>
2023-10-24 00:13:19 +1100

    added another constructor

--- src/main/java/app/totalPop.java
+++ src/main/java/app/totalPop.java
@@ -1,18 +1,24 @@
 package app;
 
 public class totalPop {
-    private String stat;
+    private String status;
     private int total2016;
     private int total2021;
+    private String state;
 
-    public totalPop(String stat, int total2016, int total2021){
-        this.stat = stat;
+    public totalPop(String status, int total2016, int total2021){
+        this.status = status;
         this.total2016 = total2016;
         this.total2021 = total2021;
     }
 
-    public String getStat(){
-        return this.stat;
+    public totalPop(String state, int total2021){
+        this.state = state;
+        this.total2021 = total2021;
+    }
+
+    public String getStatus(){
+        return this.status;
     }
 
     public int getTotal2016(){
@@ -23,4 +29,8 @@ public class totalPop {
         return this.total2021;
     }
 
+    public String getState(){
+        return this.state;
+
+    }
 }
