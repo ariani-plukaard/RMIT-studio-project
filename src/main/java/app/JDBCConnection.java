8eb1bd861e225721913c279f602cbb83a04a39e0
Ariani Plukaard <s4055430@student.rmit.edu.au>
2023-10-24 09:44:49 +1100

    added method to get categories - for the level 3 page filter drop-downs

--- src/main/java/app/JDBCConnection.java
+++ src/main/java/app/JDBCConnection.java
@@ -586,4 +586,70 @@ public class JDBCConnection {
 
         return totalPops;
     }
+
+    // Method to get the categories for each topic
+    public ArrayList<String> getCategories(String topic) {
+        // Create the ArrayList of Strings to return
+        ArrayList<String> categories = new ArrayList<String>();
+
+        // Setup the variable for the JDBC connection
+        Connection connection = null;
+
+        try {
+            // Connect to JDBC data base
+            connection = DriverManager.getConnection(DATABASE);
+
+            // Prepare a new SQL Query & Set a timeout
+            Statement statement = connection.createStatement();
+            statement.setQueryTimeout(30);
+
+            // Get category column name by topic
+            String categoryCol = "";
+            if (topic.equals("Population")) {
+                categoryCol = "age_category";
+            } else if (topic.equals("LTHC")) {
+                categoryCol = "Condition";
+            } else if (topic.equals("SchoolCompletion")) {
+                categoryCol = "SchoolYear";
+            } else if (topic.equals("NonSchoolCompletion")) {
+                categoryCol = "NonSchoolBracket";
+            }
+
+            // The Query 
+            String query = "SELECT DISTINCT " + categoryCol + " FROM " + topic + ";";
+
+            // Get Result
+            ResultSet results = statement.executeQuery(query);
+
+            // Process all of the results
+            while (results.next()) {
+                // Lookup the columns we need
+                String category    = results.getString(categoryCol);
+
+                // Add the category to the array
+                categories.add(category);
+            }
+
+            // Close the statement because we are done with it
+            statement.close();
+        } catch (SQLException e) {
+            // If there is an error, lets just pring the error
+            System.err.println(e.getMessage());
+        } finally {
+            // Safety code to cleanup
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                // connection close failed.
+                System.err.println(e.getMessage());
+            }
+        }
+
+        // Finally we return all of the categories
+        return categories;
+    }
+    
+
 }
