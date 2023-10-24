b467c2954a564b6c084662061b0e2e0b0fff7b9c
Nathan <s3993380@student.rmit.edu.au>
2023-10-24 14:08:19 +1100

    added another constructor

--- src/main/java/app/OverviewData.java
+++ src/main/java/app/OverviewData.java
@@ -7,6 +7,7 @@ public class OverviewData {
     // Data Overview table attributes, all text values
    private String location, category;
    private int count;
+   private float propCount;
 
    /**
     * Create a Data Overview object and set the fields
@@ -15,6 +16,12 @@ public class OverviewData {
       this.location = location;
       this.category = category;
       this.count = count;
+   }
+
+   public OverviewData(String location, String category, float propCount) {
+      this.location = location;
+      this.category = category;
+      this.propCount = propCount;
    }
 
    public String getLocation() {
@@ -27,5 +34,9 @@ public class OverviewData {
 
     public int getCount() {
       return count;
+   }
+
+   public float getPropCount(){
+      return propCount;
    }
 }
